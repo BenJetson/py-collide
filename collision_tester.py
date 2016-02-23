be2ebd87d41f515b0c657fe3ef71c36e8240d0fa
Ben Godfrey <bgodfr5236@greenvilleschools.us>
2016-02-23 11:25:49 -0500

    First three working.

--- collision_tester.py
+++ collision_tester.py
@@ -38,7 +38,7 @@ rectangle = [350, 275, 100, 50]
 3 = circle-circle
 4 = rectangle-rectangle
 '''
-case = 1
+case = 3
 
 # Game loop
 done = False
--- intersects.py
+++ intersects.py
@@ -20,16 +20,55 @@ the rectangle.
 
 
 def point_circle(point, circle):
-    pass
+    
+    x1 = point[0]
+    y1 = point[1]
+    x2 = circle[0]
+    y2 = circle[1]
+    r = circle[2]
+    
+    return (((x1-x2)**2) + ((y1-y2)**2)) <= r**2
+    
 
 
 def point_rect(point, rect):
-    pass
+    x = point[0]
+    y = point[1]
+    
+    x_min = rect[0]
+    y_min = rect[1]
+    x_max = rect[0] + rect[2]
+    y_max = rect[1] + rect[3]
+    
+    return (x_min <= x <= x_max) and (y_min <= y <= y_max)
 
 
 def circle_circle(circle1, circle2):
-    pass
+    
+    x1 = circle1[0]
+    y1 = circle1[1]
+    r1 = circle1[2]
+    x2 = circle2[0]
+    y2 = circle2[1]
+    r2 = circle2[2]
+    
+    return (((x1-x2)**2) + ((y1-y2)**2)) <= ((r1+r2)**2)
 
 
 def rect_rect(rect1, rect2):
-    pass
+    
+    left1 = rect1[0]
+    right1 = rect1[0] + rect1[2]
+    top1 = rect1[1]
+    bottom1 = rect1[1] + rect1[3]
+    
+    left2 = rect2[0]
+    right2 = rect2[0] + rect2[2]
+    top2 = rect2[1]
+    bottom2 = rect2[1] + rect2[3]
+    
+    return (right1 < left2 or
+            left1 < right2 or 
+            top1 < bottom2 or
+            bottom1 < top2)
+    
