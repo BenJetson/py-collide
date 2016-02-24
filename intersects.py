348016d477e9334bd85943a6c30169615b4c2776
Ben Godfrey <bgodfr5236@greenvilleschools.us>
2016-02-24 09:51:24 -0500

    All finished!

--- intersects.py
+++ intersects.py
@@ -67,8 +67,7 @@ def rect_rect(rect1, rect2):
     top2 = rect2[1]
     bottom2 = rect2[1] + rect2[3]
     
-    return (right1 < left2 or
-            left1 < right2 or 
-            top1 < bottom2 or
-            bottom1 < top2)
+    # return (right1 > left2) or (left2 < right1)# or (top1 > bottom2) or (bottom2 > top1)
+    # return not ((right1 < left2) or (top1 > bottom2) or (left1 < right2) or (bottom1 < top2))
+    return not ((right1 < left2) or (top1 > bottom2) or (left1 > right2) or (bottom1 < top2))
     
